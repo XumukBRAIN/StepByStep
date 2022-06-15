9af2becceb7b62a18be16ddcd1b2e9ca36b7a211
XumukBRAIN <xumuk_332@mail.ru>
2022-06-15 20:13:31 +0300

    Added JUnit test and written javadoc

--- src/MyArrList/MyArrayList.java
+++ src/MyArrList/MyArrayList.java
@@ -1,15 +1,38 @@
 package MyArrList;
 
-import org.junit.Test;
 
 import java.util.Arrays;
 
+/**
+ * @author Ivan Kudryashov
+ * @version 1.0
+ * */
 public class MyArrayList <T>{
+
+    /**
+     * a list containing data of a specific type
+     */
     private T[] list;
+
+    /**
+     * list size
+     */
     private int size;
+
+    /**
+     * list capacity
+     */
     private int capacity = 8;
+
+    /**
+     * current object index
+     */
     private int currentIndex = 0;
 
+    /**
+     * constructor to create a sheet with a specific capacity
+     * @param capacity list
+     */
     public MyArrayList(int capacity){
         if(capacity < 1){
             throw new IllegalArgumentException("capacitance must be equal to 1 or more");
@@ -18,10 +41,16 @@ public class MyArrayList <T>{
         }
     }
 
+    /**
+     * default constructor
+     */
     public MyArrayList(){
         list = (T[]) new Object[capacity];
     }
 
+    /**
+     * method to increase list capacity
+     */
     private void makeArrayGreatAgain() {
         int newCapacity = capacity * 3 / 2 + 1;
         Integer[] newExpandedArray = new Integer[newCapacity];
@@ -31,6 +60,10 @@ public class MyArrayList <T>{
         capacity = newCapacity;
     }
 
+    /**
+     * method for adding a new object to MyArrayList
+     * @param object you want to add
+     * */
     public void add(T object){
         //Проверка на переполнение capacity
         if(currentIndex >= list.length){
@@ -40,16 +73,28 @@ public class MyArrayList <T>{
         size++;
     }
 
+    /**
+     * method to get list size
+     * @return list size
+     */
     public int size(){
         return size;
     }
 
+    /**
+     * method for deleting an object by its index
+     * @param index of the object you want to delete
+     */
     public void delete(int index){
         for(int i = index; i < size; i++){
             list[i] = list[i+1];
         }
     }
 
+    /**
+     * a method for removing a list element, in the parameters of which the object itself is passed
+     * @param object you want to delete
+     */
     public void deleteByObject(T object){
         int position = index(object);
         if(position < 0){
@@ -58,7 +103,11 @@ public class MyArrayList <T>{
         delete(position);
     }
 
-
+    /**
+     * method to get object index
+     * @param object whose index you want to know
+     * @return object index
+     */
     private int index(T object){
         if(object == null)
             return -1;
@@ -70,14 +119,28 @@ public class MyArrayList <T>{
         return -1;
     }
 
+    /**
+     * a method to get the index of the object you want to get
+     * @param index of the object you want to get
+     * @return object
+     */
     public T getObject(int index){
         return list[index];
     }
 
+    /**
+     * object change method
+     * @param index of the object you want to change
+     * @param object you want to change
+     */
     public void setObject(int index, T object){
         list[index] = object;
     }
 
+    /**
+     * method to check the list for the presence of objects
+     * @return true if there are no objects and vice versa
+     */
     public boolean isEmpty(){
         return size == 0;
     }
--- src/MyArrList/Test.java
+++ src/MyArrList/Test.java
@@ -9,5 +9,11 @@ public class Test {
         list.add(3);
 
         System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.getObject(0));
+        System.out.println(list.isEmpty());
+        list.delete(2);
+        list.deleteByObject(2);
+        System.out.println(list);
     }
 }
